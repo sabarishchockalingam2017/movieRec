ae0d431095c1e1fed6255665e5042d8a05e95628
sabarishchockalingam2017 <sabarishchockalingam2017@u.northwestern.edu>
2019-05-22 18:44:23 -0500

    run updated to accept bucket name when uploading to s3

--- run.py
+++ run.py
@@ -6,6 +6,7 @@ logger = logging.getLogger("movierec")
 #from src.dlmovdata import load_data
 from src.model import create_sqlite_db, create_db
 from config import BUCKET_NAME, SQLALCHEMY_DATABASE_URI, DATABASE_NAME
+from src.dlmovdata import load_data
 
 
 
@@ -15,9 +16,9 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Data processes")
     subparsers = parser.add_subparsers()
 
-    #sub_process = subparsers.add_parser('loadS3')
-    #sub_process.add_argument("--bucket", type=str, default=BUCKET_NAME, help="Bucket to be copied to")
-    #sub_process.set_defaults(func=load_data)
+    sub_process = subparsers.add_parser('loadS3')
+    sub_process.add_argument("--bucket", type=str, default=BUCKET_NAME, help="Bucket to be copied to")
+    sub_process.set_defaults(func=load_data)
 
     sub_process = subparsers.add_parser('createSqlite')
     sub_process.add_argument("--engine_string", type=str, default=SQLALCHEMY_DATABASE_URI,
--- src/dlmovdata.py
+++ src/dlmovdata.py
@@ -13,35 +13,39 @@ import config
 logging.config.fileConfig(config.LOGGING_CONFIG)
 logger = logging.getLogger('data-downloader')
 
-# download movielens data
 
-dldir = os.path.join("..","data","mdatadl.zip")
-logger.info("Downloading data") 
-r = requests.get(config.DOWNLOADURL)
+def load_data(args):
+	'''Downloads data from movielens website and uploads it to specified aws s3 bucket'''
+	
+	# download movielens data
+	dldir = os.path.join("..","data","mdatadl.zip")
+	logger.info("Downloading data") 
+	r = requests.get(config.DOWNLOADURL)
 
-with open(dldir, 'wb') as f:  
-    f.write(r.content)
+	with open(dldir, 'wb') as f:  
+	    f.write(r.content)
 
-logger.info("Download Complete. Unzipping file.")
+	logger.info("Download Complete. Unzipping file.")
 
-unzipdir = os.path.join("..","data")
-zip_ref = zipfile.ZipFile(dldir, 'r')
-zip_ref.extractall(unzipdir)
-zip_ref.close()
+	unzipdir = os.path.join("..","data")
+	zip_ref = zipfile.ZipFile(dldir, 'r')
+	zip_ref.extractall(unzipdir)
+	zip_ref.close()
 
-moviesfile = os.path.join("..","data",config.EXTRACT_FOLDER,"movies.csv")
-ratingsfile = os.path.join("..","data",config.EXTRACT_FOLDER,"ratings.csv")
+	moviesfile = os.path.join("..","data",config.EXTRACT_FOLDER,"movies.csv")
+	ratingsfile = os.path.join("..","data",config.EXTRACT_FOLDER,"ratings.csv")
 
-logger.info("Uploading to S3 Bucket.")
-# Upload the files
-s3_client = boto3.client('s3')
-try:
-  response = s3_client.upload_file(moviesfile, config.UPLOAD_BUCKET, 'ml-data/movies.csv')
-except Exception as e:
-  logging.error(e)
+	logger.info("Uploading to S3 Bucket.")
 
-try:
-  response = s3_client.upload_file(ratingsfile, config.UPLOAD_BUCKET, 'ml-data/ratings.csv')
-except Exception as e:
-  logging.error(e)
-logger.info("S3 Upload Complete.")    
+	# Upload the files
+	s3_client = boto3.client('s3')
+	try:
+	  response = s3_client.upload_file(moviesfile, args.bucket, 'ml-data/movies.csv')
+	except Exception as e:
+	  logging.error(e)
+
+	try:
+	  response = s3_client.upload_file(ratingsfile, args.bucket, 'ml-data/ratings.csv')
+	except Exception as e:
+	  logging.error(e)
+	logger.info("S3 Upload Complete.")    
